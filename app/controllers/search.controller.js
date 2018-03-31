c4c0970bf53a69e908e14918259965fb058a69d9
gotoss08 <gotoss08@gmail.com>
2018-03-31 18:33:26 +0600

    implemented search

--- app/controllers/search.controller.js
+++ app/controllers/search.controller.js
@@ -1,33 +1,81 @@
-const { Post } = require('../models/post.model');
+const {Post} = require('../models/post.model');
+const Country = require('../models/country.model.js');
+const mongoose = require('mongoose');
 
-const querystring = require('querystring');
+module.exports.indexGet = (req, res, next) => {
+    return res.render('search/index', {query: req.params.query ? req.params.query : ''});
+};
 
-module.exports.index = (req, res, next) => {
+module.exports.indexPost = (req, res, next) => {
+    let page = 1;
+    if (req.body.page) page = req.body.page;
+    let itemsPerPage = 10;
 
-	const query = req.query.query;
-	const results = [];
+    Post.find({posted: true, $or: [
+            {title: {$regex: req.body.query, $options: 'i'}},
+            {subtitle: {$regex: req.body.query, $options: 'i'}},
+            {body: {$regex: req.body.query, $options: 'i'}},
+            {'markers.header': {$regex: req.body.query, $options: 'i'}},
+            {'markers.body': {$regex: req.body.query, $options: 'i'}},
+        ]}).sort({createdAt: '-1'}).skip((page-1) * itemsPerPage).limit(itemsPerPage).exec()
+        .then((posts) => {
+            data = {posts: posts, preparedPosts: []};
+            data.posts.forEach((post) => {
+                data.preparedPosts.push({
+                    id: post._id.toString(),
+                    title: post.title,
+                    subtitle: post.subtitle,
+                    body: post.body,
+                    uniqIpsVisited: post.uniqIpsVisited.length,
+                    totalVisitCount: post.totalVisitCount,
+                    likes: post.likes.length,
+                    countries: Array.from(post.countries),
+                    userId: post.userId,
+                    postedAt: new Date(post.createdAt.getTime()),
+                    editedAt: new Date(post.updatedAt.getTime()),
+                });
+            });
+            return data;
+        })
+        .then((data) => {
+            return Country.find().exec().then((countries) => {
+                data.countries = [];
+                countries.forEach((country) => {
+                    data.countries.push({
+                        id: country._id,
+                        name: country.name,
+                    });
+                });
+                return data;
+            });
+        })
+        .then((data) => {
+            data.preparedPosts.forEach((post) => {
+                let postCountryIds = [];
+                post.countries.forEach((country) => {
+                    postCountryIds.push(new mongoose.Types.ObjectId(country));
+                });
+                post.preparedCountries = [];
+                postCountryIds.forEach((postCountryId) => {
+                    data.countries.forEach((country) => {
+                        let countryId = country.id;
 
-	render = (results) => {
-		res.render('search/index', {
-			query: query ? query : '',
-			results: results
-		});
-	};
-
-	if(query) {
-		Post.find({ $text: { $search: 'hello' } }, (err, posts) => {
-			if(err) return next(err);
-
-				for(let i = 0; i < posts.length; i++) {
-					let post = posts[i];
-
-					results.push({
-						text: post.title,
-						url: '/post/' + post._id
-					});
-				}
-
-				render(results);
-		});
-	} else render();
+                        if (postCountryId.equals(countryId)) {
+                            post.preparedCountries.push({
+                                id: country.id,
+                                name: country.name,
+                            });
+                        }
+                    });
+                });
+            });
+            return data;
+        })
+        .then((data) => {
+            return res.status(200).send({posts: data.preparedPosts});
+        })
+        .catch((err) => {
+            console.error(err);
+            return res.sendStatus(400);
+        });
 };
--- app/routes/search.routes.js
+++ app/routes/search.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const searchController = require('../controllers/search.controller');
 
-router.get('/', searchController.index);
+router.get('/', searchController.indexGet);
+router.get('/:query', searchController.indexGet);
+router.post('/', searchController.indexPost);
 
 module.exports = router;

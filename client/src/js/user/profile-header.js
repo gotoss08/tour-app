039ab5fa208e907cf5aaae47b67e40acaee4701e
gotoss08 <gotoss08@gmail.com>
2018-05-26 20:33:44 +0600

    fixed showing write post button at not current user profile page

--- client/src/js/user/profile-header.js
+++ client/src/js/user/profile-header.js
@@ -8,7 +8,7 @@ function postsLoaded() {
 
 $(document).ready(() => {
     $('#header .header-page-control-section').append('<span id="posts-load-status" title="Статус загрузки постов"></span>');
-    $('#header .header-page-control-section').append('<a href="/p/new" class="header-link ml-1" title="Новая заметка"><i class="fas fa-pencil-alt"></i></a>');
+    if (profileData.currentUser) $('#header .header-page-control-section').append('<a href="/p/new" class="header-link ml-1" title="Новая заметка"><i class="fas fa-pencil-alt"></i></a>');
 
     tippy('[title]');
 
--- public/js/user/profile-header.js
+++ public/js/user/profile-header.js
@@ -10,7 +10,7 @@ function postsLoaded() {
 
 $(document).ready(function () {
     $('#header .header-page-control-section').append('<span id="posts-load-status" title="Статус загрузки постов"></span>');
-    $('#header .header-page-control-section').append('<a href="/p/new" class="header-link ml-1" title="Новая заметка"><i class="fas fa-pencil-alt"></i></a>');
+    if (profileData.currentUser) $('#header .header-page-control-section').append('<a href="/p/new" class="header-link ml-1" title="Новая заметка"><i class="fas fa-pencil-alt"></i></a>');
 
     tippy('[title]');
 
